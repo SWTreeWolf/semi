f84d32b9a4877cbe99fb8e1bd0f3838df5dbc471
123tmd123 <123tmdqja123@naver.com>
2018-06-20 18:38:18 +0900

    test

---
+++ semipro/Java001_main.java
@@ -0,0 +1,15 @@
+package java0309;
+
+// 메소드
+/*
+ * java Sample
+ * 
+ * Ctrl + F11 : 실행
+ */
+public class Java001_main {
+     public static void main(String[] args) {
+          System.out.println("Sample Test");
+     }  //프로그램 종료
+}
+
+
